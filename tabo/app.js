4f4ee3d525654df550ae7200dbef5b3618184f78
Davis Banoog <davis@gemini.local>
2025-04-06 10:11:32 +0800

    Update search URL

--- tabo/app.js
+++ tabo/app.js
@@ -153,8 +153,8 @@ function updateProgress() {
 
 function openRandomLink() {
     const randomIndex = Math.floor(Math.random() * words.length);
-    // let searchURL = `https://www.bing.com/search?q=${temp[randomIndex]}`
-    let searchURL = `https://www.bing.com/search?pglt=171&q=${temp[randomIndex]}&cvid=4f3824c6c7b7447ab4c8dc5c82eff4d7&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQABhAMgYIAhAuGEAyBggDEAAYQDIGCAQQLhhAMgYIBRAuGEAyBggGEC${counter}YQDIGCAcQRRhBMgYICBBFGEHSAQc${counter}NDBqMGoxqAIAsAIA&FORM=ANNTA1&PC=U531`;
+    let searchURL = `https://www.bing.com/search?pglt=299&q=${temp[randomIndex]}&cvid=9a7b4ff054dd4605b2ff3eacf2004a3e&gs_lcrp=EgRlZGdlKgYIABBFGDkyBggAEEUYOTIGCAEQABhAMgYIAhAAGEAyBggDEAAYQDIGCAQQABhAMgYIBRAAGEAyBggGEAAYQDIGCAcQABhAMgYICBAAGEDSAQg${counter}MzkxajBqMagCCLACAQ&FORM=ANNTA1&PC=U531`
+    // let searchURL = `https://www.bing.com/search?pglt=171&q=${temp[randomIndex]}&cvid=4f3824c6c7b7447ab4c8dc5c82eff4d7&gs_lcrp=EgZjaHJvbWUyBggAEEUYOTIGCAEQABhAMgYIAhAuGEAyBggDEAAYQDIGCAQQLhhAMgYIBRAuGEAyBggGEC${counter}YQDIGCAcQRRhBMgYICBBFGEHSAQc${counter}NDBqMGoxqAIAsAIA&FORM=ANNTA1&PC=U531`;
     const newTab = window.open(searchURL, '_blank');
     counter += 1;
     temp.splice(randomIndex, 1);
